8545f086de1e7666707c0f436ee279b6cbe906da
cara ottmar <cara.ottmar@gmail.com>
2018-04-29 00:26:23 -0700

    worked on lab some but not complete

--- src/lib/server.js
+++ src/lib/server.js
@@ -3,28 +3,67 @@
 const net = require('net');
 const logger = require('./logger');
 const faker = require('faker');
+const server = module.exports = {};
 
 const app = net.createServer();
 let clients = [];
+
+// * Create a Client constructor that models an individual connection. 
+//   * Each client instance should contain at least an `id`, `nickname`, and `socket`.
+function Client(id, nickname, socket) {
+  this.socket = socket,
+  this.id = id,
+  this.nickname = nickname
+}
+
+// * DONE The client should send `@quit` to disconnect
+//   * DONE The client should send `@list` to list all connected users
+//   * The client should send `@nickname <new-name>` to change their nickname
+  // * The client should send `@dm <to-username> <message>` to  send a message directly to another user by nickname
 
 const parseCommand = (message, socket) => {
   if(!message.startsWith('@')) {
     return false; 
   }
-
   const parsedMessage = message.split(' ');
   const command = parsedMessage[0];
-  logger.log(logger.INFO, `Parksing a command ${command}`);
+  logger.log(logger.INFO, `Parsing a command ${command}`);
 }
-
+// O(n) time and O(n) space using .map but we can use forEach
+// @dm and default: from lecture
 switch (command) {
   case '@list': {
     const clientNames = clients.map(client => client.name).join('\n');
     socket.write(`${clientNames}\n`);
     break;
   }
+
+  case '@quit': {
+    removeClient(socket)();
+    socket.destroy();
+    break;
+  }
+  case '@nickname': {
+    clients.forEach((client) => {
+      if( client.name === parsedMessage[1]) {
+        client.nickname = parsedMessage[1];
+      }
+    });
+    break;
+  }
+  case '@dm': {
+    client.forEach((client) => {
+      if (client.socket == socket) {
+        client.socket.write(`${parsedMessage[2]}\n`);
+      }
+    });
+    break;
+  }
+  default:
+    socket.write('Invalid');
+    break;
+}
   return true;
-};
 
 const removeClient = socket => () => {
   clients = clients.filter(client => client !== socket);
